097b7e67dc8e9f90178ba0459a65d12d7759cd1e
t66587979-dotcom <t66587979@gmail.com>
2026-02-10 03:16:16 +0600

    Update info.js

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -1,5 +1,17 @@
+module.exports.config = {
+ name: "info",
+ version: "1.0.0",
+ hasPermssion: 0,
+ credits: "HRIDOY",
+ description: "Bot information command",
+ commandCategory: "For users",
+ hide: true,
+ usages: "",
+ cooldowns: 5,
+};
+
 module.exports.run = async function ({ api, event, args, Users, Threads }) {
- const { threadID, messageID } = event;
+ const { threadID } = event;
  const request = global.nodemodule["request"];
  const fs = global.nodemodule["fs-extra"];
  const moment = require("moment-timezone");
@@ -20,27 +32,6 @@ module.exports.run = async function ({ api, event, args, Users, Threads }) {
  const totalUsers = global.data.allUserID.length;
  const totalThreads = global.data.allThreadID.length;
 
- // 🔥 LOADING ANIMATION START
- const loadingSteps = [
-  "█░░░░░░░░░ 10%",
-  "███░░░░░░░ 30%",
-  "█████░░░░░ 50%",
-  "███████░░░ 70%",
-  "█████████░ 90%",
-  "██████████ 100% ✨"
- ];
-
- let loadMsg = await api.sendMessage("🔄 Loading...\n\n" + loadingSteps[0], threadID);
-
- for (let i = 1; i < loadingSteps.length; i++) {
-  await new Promise(resolve => setTimeout(resolve, 500));
-  await api.editMessage(
-   "🔄 Loading...\n\n" + loadingSteps[i],
-   loadMsg.messageID
-  );
- }
- // 🔥 LOADING ANIMATION END
-
  const msg = `╭⭓ ⪩ 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 ⪨
 │
 ├─ 🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲 : ✦ 𝙏𝙊𝙍𝙐 𝘾𝙃𝘼𝙉 ✦
@@ -48,6 +39,18 @@ module.exports.run = async function ({ api, event, args, Users, Threads }) {
 ├─ ♻️ 𝗣𝗿𝗲𝗳𝗶𝘅 𝗕𝗼𝘅 : ${prefix}
 ├─ 🔶 𝗠𝗼𝗱𝘂𝗹𝗲𝘀 : ${commands.size}
 ├─ 🔰 𝗣𝗶𝗻𝗴 : ${Date.now() - event.timestamp}ms
+│
+╰───────⭓
+
+╭⭓ ⪩ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ⪨
+│
+├─ 👑 𝗡𝗮𝗺𝗲 : 𝗞𝗮𝗸𝗮𝘀𝗵𝗶 𝗛𝗮𝘁𝗮𝗸𝗲
+├─ 📲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
+│ facebook.com/100061935903355
+├─ 💌 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
+│ m.me/100061935903355
+├─ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
+│ wa.me/+8801744-******
 │
 ╰───────⭓
 
@@ -61,17 +64,19 @@ module.exports.run = async function ({ api, event, args, Users, Threads }) {
 ❤️ 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘂𝘀𝗶𝗻𝗴 🌺
 `;
 
- const imgLink = "https://i.imgur.com/oEh5VEx.jpeg";
+ const imgLinks = [
+ "https://i.imgur.com/oEh5VEx.jpeg"
+ ];
+
+ const imgLink = imgLinks[Math.floor(Math.random() * imgLinks.length)];
 
  const callback = () => {
-  api.deleteMessage(loadMsg.messageID);
-  api.sendMessage({
-   body: msg,
-   attachment: fs.createReadStream(__dirname + "/cache/info.jpg")
-  }, threadID, () => fs.unlinkSync(__dirname + "/cache/info.jpg"));
+ api.sendMessage({
+ body: msg,
+ attachment: fs.createReadStream(__dirname + "/cache/info.jpg")
+ }, threadID, () => fs.unlinkSync(__dirname + "/cache/info.jpg"));
  };
 
- return request(encodeURI(imgLink))
-  .pipe(fs.createWriteStream(__dirname + "/cache/info.jpg"))
-  .on("close", callback);
+ return request(encodeURI(imgLink)).pipe(fs.createWriteStream(__dirname + "/cache/info.jpg")).on("close", callback);
 };
+ 
