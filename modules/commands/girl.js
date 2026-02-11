9b49cecd5184c24ecf992997276ab0dfdc703621
t66587979-dotcom <t66587979@gmail.com>
2026-02-12 00:56:33 +0600

    Update girl.js

--- modules/commands/girl.js
+++ modules/commands/girl.js
@@ -23312,7 +23312,7 @@ var min = Math.floor(Math.random() * 2);
   var money = data.money
       if(money < 200) api.sendMessage("Do you need $200 to see the photo?",event.threadID,event.messageID)
           else {
-   Currencies.setData(event.senderID, options = {money: money - 25000})
+   Currencies.setData(event.senderID, options = {money: money - 200})
    var callback = () => api.sendMessage({body:`🙉⚜️ Photo: ${link.length}\n💸 -$200!`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
      }
