a64d3a2383fcd8524475686e5eff534e5cea05fe
t66587979-dotcom <t66587979@gmail.com>
2026-02-11 16:42:18 +0600

    Update arrest.js

--- modules/commands/arrest.js
+++ modules/commands/arrest.js
@@ -52,8 +52,8 @@ async function makeImage({ one, two }) {
  let avatarTwo = `${__root}/avt_${two}_${randomID}.png`;
 
  // Public profile picture (tokenless)
- const avatarUrlOne = `https://graph.facebook.com/${one}/picture?width=512&height=512`;
- const avatarUrlTwo = `https://graph.facebook.com/${two}/picture?width=512&height=512`;
+ const avatarUrlOne = `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+ const avatarUrlTwo = `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
  // Try download, use fallback if fail
  try {
