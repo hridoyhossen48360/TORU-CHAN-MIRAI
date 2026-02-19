97733bf40224da7fa8a4c4eab81ac00fdf448afb
kakashihatake420000h-cloud <kakashihatake420000h@gmail.com>
2026-02-19 23:48:24 +0600

    Update anipic.js

--- modules/commands/anipic.js
+++ modules/commands/anipic.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "anipic",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝙈𝙧𝙏𝙤𝙢𝙓𝙭𝙓",
+    credits: "Hridoy",
     description: "anilist",
     commandCategory: "Image",
     usages: "send message",
